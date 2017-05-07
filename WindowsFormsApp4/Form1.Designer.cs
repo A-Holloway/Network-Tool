2245e78d085ba42d45eba042ce7f1725ce47b9fb
adam holloway <adam80_460@msn.com>
2017-05-07 14:14:35 -0700

    Initial Commit

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroTile2 = new MetroFramework.Controls.MetroTile();
+            this.metroStyleManager1 = new MetroFramework.Components.MetroStyleManager(this.components);
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.metroPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.Location = new System.Drawing.Point(23, 64);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(114, 79);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Ping";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile1.Click += new System.EventHandler(this.metroTile1_Click);
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.CustomBackground = true;
+            this.metroTextBox1.Location = new System.Drawing.Point(26, 43);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(257, 23);
+            this.metroTextBox1.TabIndex = 1;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Location = new System.Drawing.Point(26, 72);
+            this.metroTextBox2.Multiline = true;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.Size = new System.Drawing.Size(372, 329);
+            this.metroTextBox2.TabIndex = 2;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = true;
+            this.metroButton1.Location = new System.Drawing.Point(289, 43);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(109, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "Run";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.BackColor = System.Drawing.Color.White;
+            this.metroPanel1.Controls.Add(this.metroLabel1);
+            this.metroPanel1.Controls.Add(this.metroTextBox2);
+            this.metroPanel1.Controls.Add(this.metroTextBox1);
+            this.metroPanel1.Controls.Add(this.metroButton1);
+            this.metroPanel1.CustomBackground = true;
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(143, 63);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(420, 424);
+            this.metroPanel1.TabIndex = 4;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.BackColor = System.Drawing.Color.White;
+            this.metroLabel1.CustomBackground = true;
+            this.metroLabel1.Location = new System.Drawing.Point(26, 21);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(71, 19);
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "IP Address";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // metroTile2
+            // 
+            this.metroTile2.BackColor = System.Drawing.Color.Maroon;
+            this.metroTile2.CustomBackground = true;
+            this.metroTile2.Location = new System.Drawing.Point(23, 162);
+            this.metroTile2.Name = "metroTile2";
+            this.metroTile2.Size = new System.Drawing.Size(114, 79);
+            this.metroTile2.TabIndex = 5;
+            this.metroTile2.Text = "Trace";
+            this.metroTile2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.metroTile2.Click += new System.EventHandler(this.metroTile2_Click);
+            // 
+            // metroStyleManager1
+            // 
+            this.metroStyleManager1.Owner = this;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Dark",
+            "Light"});
+            this.metroComboBox1.Location = new System.Drawing.Point(442, 28);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(121, 29);
+            this.metroComboBox1.TabIndex = 6;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 510);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.metroTile2);
+            this.Controls.Add(this.metroPanel1);
+            this.Controls.Add(this.metroTile1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Network Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.metroStyleManager1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTile metroTile2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Components.MetroStyleManager metroStyleManager1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.StyleManager = metroStyleManager1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setting Default ComboBox Setting
+            metroComboBox1.SelectedIndex = 1;
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+            // Hide/show panel when tile is selected
+            metroPanel1.Visible = Visible;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+
+        {
+            if (string.IsNullOrEmpty(metroTextBox1.Text))
+            {
+                // Checking for IP Addess in entered
+                metroTextBox1.AppendText("Enter IP Address");
+                metroTextBox1.BackColor = Color.Red;
+
+            }
+            else
+            {
+                // Clearing textbox before next ping
+                metroTextBox2.Clear();
+                // If IP text bos is empty notify
+                metroTextBox1.BackColor = default(Color);
+
+                // Get Start Ping Proess
+                ProcessStartInfo getPing = new ProcessStartInfo("cmd");
+                getPing.UseShellExecute = false;
+                getPing.RedirectStandardOutput = true;
+                getPing.CreateNoWindow = true;
+                getPing.RedirectStandardInput = true;
+                var proc = Process.Start(getPing);
+                proc.StandardInput.WriteLine($"ping " + metroTextBox1.Text);
+                proc.StandardInput.WriteLine("exit");
+
+                // Return console output to string and enter into textbox 
+                string s = proc.StandardOutput.ReadToEnd();
+                metroTextBox2.Text = s;
+            }
+
+
+        }
+
+        private void metroTile2_Click(object sender, EventArgs e)
+        {
+                // hide/show panel
+
+                metroPanel1.Visible = !Visible;
+        }
+
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Setting theme from comboBox
+            switch (metroComboBox1.SelectedIndex)
+            {
+                case 0:
+                metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Dark;
+                    metroPanel1.BackColor = Color.Black;
+                    metroPanel1.BorderStyle = MetroFramework.Drawing.MetroBorderStyle.FixedSingle;
+                    metroLabel1.BackColor = Color.Black;  
+                break;
+
+                case 1:
+                    metroStyleManager1.Theme = MetroFramework.MetroThemeStyle.Light;
+                    metroPanel1.BackColor = Color.White;
+                    metroLabel1.BackColor = Color.White;
+                    break;
+
+            }
+            
+        }
+
+    }
+    } 
